7356a172a9e9f6443e520c0bf27a6d231a69db9a
Sublime HTML5 <raphiengai@gmail.com>
2022-04-09 13:00:33 +0800

    first commit

---
+++ Lesson 1/app.js
@@ -0,0 +1,13 @@
+console.log("I m koKOnUt");
+$("#light").click(function(){
+$("body").css("background-color","powderblue");
+$(".boxes").css("background-color","cornsilk");
+$(".content").css("background-color","peachpuff");
+$("body").css("color","navy");
+})
+$("#dark").click(function(){
+$("body").css("background-color","black");
+$(".boxes").css("background-color","navy");
+$(".content").css("background-color","indigo");
+$("body").css("color","white");
+})
